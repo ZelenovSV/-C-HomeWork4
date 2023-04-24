e22d0134dc31ea42bade4e149cf3f83a920ddeb6
Sergey Zelenov <S.Zelenov23@mail.ru>
2023-04-24 22:50:37 +0300

    задача 25

---
+++ задача 25.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число А: ");
+    int A = int.Parse(Console.ReadLine());
+    double function(int number)
+    {
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    sum = sum + A[i];
+    return sum;
+    }
+    Console.WriteLine($"Сумма чисел: {function(A)}");
+    
